97b7719100ea6190217a6a9e162227001d72fe21
xudafeng <xudafeng@126.com>
2017-04-10 21:25:13 +0800

    fix libs output

--- scripts/find-opencv.js
+++ scripts/find-opencv.js
@@ -10,5 +10,5 @@ exec(`pkg-config "opencv >= opencv_version" ${flag}`, (error, stdout, stderr) =>
   if (error) {
     throw new Error('ERROR: failed to run: pkg-config');
   }
-  console.log(stdout);
+  console.log(stdout.trim());
 });
